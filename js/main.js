1f9368f9ed49d595350769aabd9615661b159e30
Marilia Bessa <mariliambessa@gmail.com>
2019-07-01 20:16:13 -0300

    JS variavel inpute dentro da função

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     const button = $("#input-button");
-    let username = $("#input-user").val();
-
-    button.click(function(){
-       alert(); 
+    
+    button.click(function(){ //quando clicar o botão, a função nas chaves será executada;
+        let username = $("#input-user").val();
+        alert(username); 
     });
 
 
