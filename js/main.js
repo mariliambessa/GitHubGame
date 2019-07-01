7bf92e4ddaa11ecbfc6ce343f018c474c221eea3
Marilia Bessa <mariliambessa@gmail.com>
2019-07-01 20:36:06 -0300

    .getJSON

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
     const button = $("#input-button");
-    
+    const url = 'https://api.github.com/users/'
+
     button.click(function(){ //quando clicar o botão, a função nas chaves será executada;
-        let username = $("#input-user").val();
-        alert(username); 
+        let username = $("#input-user").val(); //adicionar a variavel no evento pois não há entrada no input até que se clique
+        $.getJSON(url + username, function(data){
+            console.log(data);
+        }) //de onde quero tirar o JSON? url + o que foi digitado no input
+        
+        alert(data);
+    
     });
 
 
